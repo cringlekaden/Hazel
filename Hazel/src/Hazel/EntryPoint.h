c6a35b98dd01e72e0915f470abe8736e13741056
Kaden <cringlekaden@gmail.com>
2026-09-16 15:44:16 -0400

    Added logging via spdlog.

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #if defined(HZ_PLATFORM_WINDOWS) || defined(HZ_PLATFORM_LINUX)
+
 extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
+    Hazel::Log::Init();
+    HZ_CORE_WARN("Initialized Log!");
     auto app = Hazel::CreateApplication();
     app->Run();
     delete app;
