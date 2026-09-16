0769e3fd765e500b1e5dc99c0c0f222c992f7069
Kaden <cringlekaden@gmail.com>
2026-09-16 15:09:37 -0400

    Missed one change in last commit...

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -14,10 +14,7 @@ public:
     }
 };
 
-int main()
+Hazel::Application* Hazel::CreateApplication()
 {
-    Sandbox* sandbox = new Sandbox();
-    sandbox->Run();
-    delete sandbox;
-    return 0;
+    return new Sandbox();
 }
